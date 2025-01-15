ae578b491ce061d9bca66f013576aa9637d90110
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-01-15 23:58:43 +0100

    feat: add repository for user profiles

---
+++ internal/profiles/repository_profiles.go
@@ -0,0 +1,45 @@
+package profiles
+
+import (
+	"context"
+	"time"
+
+	"github.com/growteer/api/pkg/web3util"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Profile struct {
+	DID web3util.DID `bson:"_id"`
+	FirstName string `bson:"firstName"`
+	LastName string `bson:"lastName"`
+	PrimaryEmail string `bson:"primaryEmail"`
+	Location struct {
+		Country string `bson:"country"`
+		PostalCode string `bson:"postalCode"`
+		City string `bson:"city"`
+	} `bson:"location,omitempty"`
+	Website string `bson:"website,omitempty"`
+	PersonalGoal string `bson:"personalGoal,omitempty"`
+	About string `bson:"about,omitempty"`
+	CreatedAt time.Time `bson:"createdAt"`
+}
+
+func (r *repository) GetByDID(ctx context.Context, did web3util.DID) (*Profile, error) {
+	var result *Profile
+	err := r.profiles.FindOne(ctx, bson.M{"_id": did.String()}).Decode(result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (r *repository) Create(ctx context.Context, profile Profile) (*Profile, error) {
+	profile.CreatedAt = time.Now()
+	_, err := r.profiles.InsertOne(ctx, profile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &profile, nil
+}
