88bfaffc8610bdd9aaad5775f66d284e32ac7036
Christian Brennecke-Räther <c.b.raether@gmail.com>
2025-02-28 22:24:51 +0100

    tests: solana utils

--- pkg/web3util/solana_test.go
+++ pkg/web3util/solana_test.go
@@ -4,38 +4,32 @@ import (
 	"testing"
 
 	"github.com/growteer/api/pkg/web3util"
+	"github.com/growteer/api/testing/fixtures"
+	"github.com/mr-tron/base58"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_VerifySolanaPublicKey(t *testing.T) {
 	t.Run("valid public key", func(t *testing.T) {
 		// given
-		publicKey := "2dqNNQ7jcQpjw15F89KoppKjRCD2U1Ei3eBhUJdYwf7p"
+		_, pubKey := fixtures.GenerateEd25519KeyPair(t)
 
 		// when
-		err := web3util.VerifySolanaPublicKey(publicKey)
+		err := web3util.VerifySolanaPublicKey(pubKey)
 
 		// then
 		assert.NoError(t, err)
 	})
 
-	t.Run("too long key", func(t *testing.T) {
+	t.Run("non-Base58 public key", func(t *testing.T) {
 		// given
-		longPublicKey := "ASXcnp9AJmewtxFvK8iBEwnJe6oJbs348jZqtgdu9dNWtMV8bEPqszg"
+		_, pubKey := fixtures.GenerateEd25519KeyPair(t)
+		pubKeyDecoded, err := base58.Decode(pubKey)
+		require.NoError(t, err)
 
 		// when
-		err := web3util.VerifySolanaPublicKey(longPublicKey)
-
-		// then
-		assert.Error(t, err)
-	})
-
-	t.Run("non-Base58 key", func(t *testing.T) {
-		// given
-		unencodedPublicKey := "34b25777224a34b562bdd992db51456603958892e18cfe50cef7d3fcf3b8e8c8"
-
-		// when
-		err := web3util.VerifySolanaPublicKey(unencodedPublicKey)
+		err = web3util.VerifySolanaPublicKey(string(pubKeyDecoded))
 
 		// then
 		assert.Error(t, err)
